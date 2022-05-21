c447c4cb76058cdd1f37eb391e7658e0ea467e10
Humm <jhumm@mcw.edu>
2022-05-21 11:16:53 -0500

    fix: Changed inventory_quantity in Product Class to DecimalField

--- products/models.py
+++ products/models.py
@@ -7,5 +7,5 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    inventory_quantity = models.ImageField()
+    inventory_quantity = models.DecimalField()
     web_pic = models.URLField(max_length=200)
